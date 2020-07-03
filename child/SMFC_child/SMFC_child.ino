316bb3356492f7216436f04c64486d23011339ef
Alex Netsch <alex.netsch97@gmail.com>
2020-07-03 16:13:28 +0200

    Send int(10*(distance)) to get 1 decimal place

--- child/SMFC_child/SMFC_child.ino
+++ child/SMFC_child/SMFC_child.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
-#include <HCSR04.h>
+#include <arduino.h>
+//#include <HCSR04.h>
 
 // PIN constants to avoid confusion
 static const uint8_t D0   = 16;
@@ -19,18 +20,22 @@ int ins = 0;
 // Wifi variables
 const char* ssid     = "LSRCT";
 const char* password = "83067046472468411597";
-const char* host = "192.168.178.20";
+const char* host = "192.168.178.27";
 const uint16_t port = 9999;
 void connect_wifi();
 void exec_ins(int *);
 
+int measureDistanceCm(const uint8_t *, const uint8_t *);
+
 //UltraSonicDistanceSensor distanceSensor_1(D1, D2);
 //String s1name = "S1;";
-UltraSonicDistanceSensor distanceSensor_2(D5, D6);
+//UltraSonicDistanceSensor distanceSensor_2(D5, D6);
 //String s2name = "S2;";
 WiFiClient client;
 
 void setup() {
+  pinMode(D5, OUTPUT);
+  pinMode(D6, INPUT);
   Serial.begin(115200);
   connect_wifi();
   pinMode(D7, OUTPUT);
@@ -42,12 +47,13 @@ void setup() {
 void loop() {
   if (client.connect(host, port)) {
       //client.print(s1name+distanceSensor_1.measureDistanceCm());
-      client.print(distanceSensor_2.measureDistanceCm());
+      //client.print(distanceSensor_2.measureDistanceCm());
+      client.println(measureDistanceCm(&D5,&D6));
       delay(10);
       while (client.available()) {
         ins = static_cast<int>(client.read());
         exec_ins(&ins);
-      delay(190);
+      delay(90);
       //client.print(5);
       client.flush();}
   } else {
@@ -87,3 +93,24 @@ void connect_wifi(){
   Serial.println("");
   Serial.println("WiFi connected");  
   }
+
+int measureDistanceCm(const uint8_t *triggerPin, const uint8_t *echoPin) {
+    // Make sure that trigger pin is LOW.
+    digitalWrite(*triggerPin, LOW);
+    delayMicroseconds(2);
+    // Hold trigger for 10 microseconds, which is signal for sensor to measure distance.
+    digitalWrite(*triggerPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(*triggerPin, LOW);
+    // Measure the length of echo signal, which is equal to the time needed for sound to go there and back.
+    double durationMicroSec = (double)(pulseIn(*echoPin, HIGH));
+
+    double speedOfSoundInCmPerMs = 0.03313 + 0.0000606 * 22.0; // Cair ≈ (331.3 + 0.606 ⋅ ϑ) m/s
+    double distanceCm = durationMicroSec / 2.0 * speedOfSoundInCmPerMs;
+    if (distanceCm == 0 || distanceCm > 400) {
+        return -1.0 ;
+    } else {
+        return distanceCm*10.0;
+    }
+}
+  
