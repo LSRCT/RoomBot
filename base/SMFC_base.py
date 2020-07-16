059b935276e8ed3f371086fb686beca85e7d6f16
Alex <alex.netsch97@gmail.com>
2020-07-16 12:06:30 +0200

    Added funciton to save the data

--- base/SMFC_base.py
+++ base/SMFC_base.py
@@ -1,3 +1,4 @@
+import time
 import os, sys
 import socket
 import numpy as np
@@ -12,9 +13,12 @@ class SMFCBase:
         self.websocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, fileno=None)
         self.websocket.bind((ip, port))
         self.back_count = 0
+        self.logfile = 0
 
     def __del__(self):
         self.websocket.close()
+        if self.logfile:
+            self.logfile.close()
         print("Server stopped")
 
     def serve(self):
@@ -25,9 +29,17 @@ class SMFCBase:
         self.websocket.listen()
         while 1:
             conn, addr = self.websocket.accept()
-            #print(f"Connection from address {addr}")
-            # start a new thread for every connection
             self.handle_request(conn)
+
+    def savelocdata(self, data1, data2, data3, ins):
+        if not os.path.isfile("rb_datalog.csv"):
+            with open("rb_datalog.csv", "w") as lf:
+                lf.write("t;s1;s2;s3;ins;\\n")
+        if not self.logfile:
+            self.logfile = open("rb_datalog.csv", "a")
+        self.logfile.write(";".join([str(x) for x in [time.time(), data1, data2, data3, ins, "\\n"]]))
+        
+            
 
     def handle_request(self, request):
         """
@@ -39,15 +51,16 @@ class SMFCBase:
         dist1 = (int(data_rcv[0:4], 16))/10
         dist2 = (int(data_rcv[4:8], 16))/10
         dist3 = (int(data_rcv[8:12], 16))/10
-        ins = self.handle_locdata(dist1, dist2)
+        ins = self.handle_locdata(dist1, dist2, dist3)
         #ins = b"2"
         request.send(ins)
+        self.savelocdata(dist1, dist2, dist3, ins)
         print(dist1, dist2, dist3)
         request.close()
 
-    def handle_locdata(self, dat1, dat2):
+    def handle_locdata(self, dat1, dat2, dat3):
         # 5 stop and go cycles -> ~180 deg turn
-        if dat2 < 40:
+        if dat2 < 40 or dat3 < 40:
             fak = 2
         else:
             fak = 1
