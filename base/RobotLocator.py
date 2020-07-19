b3fa493e45cc87cc7a722a02a7530b3d4fdec932
Alex <alex.netsch97@gmail.com>
2020-07-19 18:02:29 +0200

    Changed the way weights get updated to be far more efficient:

--- base/RobotLocator.py
+++ base/RobotLocator.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sklearn.preprocessing import minmax_scale
+from scipy.spatial import distance
 import pickle
 import time
 from PIL import Image
@@ -6,19 +8,17 @@ import matplotlib.pyplot as plt
 
 class RobotLocator:
     def __init__(self):
-        self.img_room = Image.open("map5.png").convert("L")
+        self.img_room = Image.open("maps//map5.png").convert("L")
         self.precalc_dist = self.load_precalc_dist()
         self.precalc_dist[1] = np.abs(self.precalc_dist[1].T[:3].T)
         self.loc_weights = np.zeros(len(self.precalc_dist[1]))
-        self.pos_believe = np.array([0,0,0])
+        self.pos_believe = np.array([920,735,0])
         self.dist_list = 0
     
     def load_precalc_dist(self):
         """ Load precalculated distances for a number of points from a pickle file"""
-        pcdist = pickle.load(open("precalc_dist_Map5.p", "rb"))
+        pcdist = pickle.load(open("precalc_distances//precalc_dist_Map5.p", "rb"))
         return pcdist
-
-
 
     def calc_dist_list(self, sensor_data, return_list=1):
         """
@@ -27,9 +27,8 @@ class RobotLocator:
         :param return_list: If true returns while list, else just returns best 10 fitting positions
         """
         positions, distances, angles = self.precalc_dist
-        dist_from_sim = np.linalg.norm((distances - sensor_data), axis=1)
-        self.dist_list = dist_from_sim
-    
+        self.dist_list = distance.cdist([sensor_data], distances, metric="mahalanobis")
+
     def get_pos_from_ind(self, ind):
         """
         Return x,y coordinates of position with index ind
@@ -39,35 +38,15 @@ class RobotLocator:
         return state
     
     def estimate_pos(self, sens_data):
-        self.calc_dist_list(sens_data)
+        self.calc_dist_list(np.array(sens_data))
         self.update_weights()
         self.pos_believe= self.get_pos_from_ind(np.argmax(self.loc_weights))
         return self.pos_believe
 
     def update_weights(self):
-        """
-        Each location is a asigned a weitght based on the last measured sensor data, this function updates that list
-        :param min_pos_list: List with possible positions ordered according to euclidean distance to measurement
-        """
-        #for mind_numb in range(10):
-        #    min_ind = np.where(dist_from_sim == np.min(dist_from_sim))[0][0]
-        #    dist_from_sim[min_ind] = np.infty
-        #    min_pos_list.append([positions[min_ind%len(positions)], distances[min_ind], angles[min_ind//len(positions)]])
-
-        # Sort the possible positions by their similarity to measured sensor data
-        sorted_dist_list = sorted(enumerate(self.dist_list), key = lambda x: x[1])
-        sorted_ind_list = (np.array(sorted_dist_list).T[0].T).astype(int)
-
-        new_weight = np.linspace(1,-1, len(sorted_ind_list)) 
-        new_weight[:500] = new_weight[:500]**2
-
-        # Sort the top100 sensor dist fits by their distance to the last estimated position
-        dist_100toLast = np.linalg.norm(np.array([self.get_pos_from_ind(x)[:2] for x in sorted_ind_list[:100]])- self.pos_believe[:2], axis=1)
-        sorted_ind_list[:100] = np.array(sorted(zip(dist_100toLast, sorted_ind_list), key=lambda x:x[0])).T[1].T
-        
-        # Give each position a new weight based on its positin in the sorted_ind_list
-        new_est_sorted = np.array(sorted(zip(new_weight, sorted_ind_list), key=lambda x: x[1])).T[0].T
-        self.loc_weights = 0.5* new_est_sorted + self.loc_weights
+        dist_similarity = 1/(1+self.dist_list)
+        self.loc_weights = (0.1 * dist_similarity + 0.9 * self.loc_weights) / 2
+        print(np.max(self.loc_weights))
 
     def plot_pos_dist(self, state):
         """
@@ -95,3 +74,4 @@ if __name__ == "__main__":
     res = rb_loc.estimate_pos(test_sensordat)
     print(f"Groundtruth: {test_groundtruth} Estimate: {res}")
     print(f"Location took {time.time()-t0} seconds")
+
