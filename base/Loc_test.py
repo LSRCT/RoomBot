d475026a0bdc986b583ee2c3aff230d5e78529ff
Alex Netsch <alex.netsch97@gmail.com>
2020-09-25 17:50:15 +0200

    Cleanup and documentation

--- base/Loc_test.py
+++ base/Loc_test.py
@@ -12,6 +12,9 @@ class RBLocTester:
         self.prob_dist = []
 
     def load_loc_data(self, logfile):
+        """
+        Load recorded sensor data from csv file
+        """
         sdat_list = []
         with open(logfile, "r") as f:
             for line in f:
@@ -20,17 +23,21 @@ class RBLocTester:
         return sdat_list
 
     def preprocess_locdata(self):
+        """
+        Apply moving average to compensate for outliers
+        """
         n = len(self.sdat_list)
         self.sdat_list = np.array(self.sdat_list).T
-        #self.sdat_list = uniform_filter1d(self.sdat_list, size=100, axis=0)
         self.sdat_list[0][5:] = winAvg(self.sdat_list[0], winWidth=10)[5:]
         self.sdat_list[1][5:] = winAvg(self.sdat_list[1], winWidth=10)[5:]
         self.sdat_list[2][5:] = winAvg(self.sdat_list[2], winWidth=10)[5:]
-        #self.sdat_list[3][5:] = winAvg(self.sdat_list[3], winWidth=10)[5:]
-        print(len(self.sdat_list))
         self.sdat_list = np.array(self.sdat_list).T
 
     def estloc_datarange(self, datslice):
+        """
+        Estimate probability maps for all datapoints
+        :param dataslice: (start, stop) data range to estimate
+        """
         est = []
         rshap = (len(set(self.rl.precalc_dist[0].T[2])), -1)
         for data_numb, data in enumerate(self.sdat_list[datslice]):
@@ -38,19 +45,19 @@ class RBLocTester:
                 print(f"Estimated {data_numb} locations")
             for noisy_sens in self.apply_noise(data[1]+11, data[0]+11, data[2]+11, data[3]):
                 est.append(self.rl.estimate_pos(noisy_sens))
-            #est.append(self.rl.estimate_pos([data[1]+12, data[0]+12, data[2]+12]))
             l_w = np.sum(np.reshape(self.rl.loc_weights, rshap), axis=0)
             self.prob_dist.append(l_w)
-            print(np.shape(l_w))
         est = np.array(est)
         self.est_pos = est
         return self.est_pos
 
     def apply_noise(self, x,y,z, phi):
+        """
+        Augment the data by applying a fixed amount of offset on every axis
+        """
         return [[x,y,z, phi]]
         new_coords = []
         noiserange = np.linspace(-0.5,0.5,3)
-        print(noiserange)
         for noise_x in noiserange:
             for noise_y in noiserange:
                 for noise_z in noiserange:
@@ -58,11 +65,17 @@ class RBLocTester:
         return new_coords
 
     def est_prob_video(self,datslice):
+        """
+        Show the probabiliy map for a dataslice as a video
+        """
+        # load simulated distances and room map
         pos = self.rl.precalc_dist[0]
         pos = np.reshape(pos, (len(set(pos.T[2])), -1, 3))[0]
         rshap = (len(set(self.rl.precalc_dist[0].T[2])), -1)
         map_room = np.asarray(self.map_room)
         map_room = np.abs(np.round(map_room, 1)-255)*255
+
+        # estimate position for all selected data points
         for data_numb, data in enumerate(self.sdat_list[datslice]):
             self.rl.estimate_pos([data[1]+11, data[0]+11, data[2]+11, data[3]])
             l_w = np.sum(np.reshape(self.rl.loc_weights, rshap), axis=0)
@@ -76,31 +89,60 @@ class RBLocTester:
                 break
         cv2.destroyAllWindows()
 
-    def plot_est_prob(self):
+    def plot_est_prob(self, frame, ax=None):
+        """
+        Plot the probability map for a single datapoint
+        :param frame: index of datapoint to plot
+        :param ax: matplotlib axis to plot to, if none show image
+        """
+        # load simulated distances and room map
         pos = self.rl.precalc_dist[0]
-        map_room = np.asarray(self.map_room)
-        map_room = np.abs(np.round(map_room, 1)-255)*255
-        self.prob_dist = np.sum(self.prob_dist, axis=0)
-        self.prob_dist = (self.prob_dist/np.max(self.prob_dist))*255
-        print(np.sum(self.prob_dist))
-        print(np.shape(self.prob_dist), np.shape(pos))
-        bs= 2 # blobsize
         pos = np.reshape(pos, (len(set(pos.T[2])), -1, 3))[0]
-        for coord, prob in zip(pos, self.prob_dist):
-            map_room[coord[0]-bs:coord[0]+bs+1, coord[1]-bs:coord[1]+bs+1] = np.ones(((bs*2)+1,(bs*2)+1), dtype=np.uint8)* np.uint8(prob)
+        map_room = np.asarray(self.map_room)
+        map_room = np.abs(np.round(map_room, 1)-255)
+
+        # estimate position from data
+        data = self.sdat_list[frame]
+        self.rl.estimate_pos([data[1]+11, data[0]+11, data[2]+11, data[3]])
+        rshap = (len(set(self.rl.precalc_dist[0].T[2])), -1)
+        l_w = np.sum(np.reshape(self.rl.loc_weights, rshap), axis=0)+0.001
+        prob = (l_w/np.max(l_w))*255
+
+        # Plot the probability map
+        bs= 2 # blobsize
+        for coord, p in zip(pos, prob):
+            map_room[coord[0]-bs:coord[0]+bs+1, coord[1]-bs:coord[1]+bs+1] = np.ones(((bs*2)+1,(bs*2)+1), dtype=np.uint8)* np.uint8(p)
         map_room_crop = map_room[550:1000, 650:1250]
-        plt.matshow(map_room_crop)
-        #plt.scatter(pos[:,1], pos[:,0], 100, self.prob_dist, marker="s")
-        
+        if ax == None:
+            plt.matshow(map_room_crop)
+            plt.show()
+        else:
+            ax.matshow(map_room_crop)
+
+    
+    def plot_prob_timeline(self, plot_ind):
+        """
+        Plot multiple probability maps in a row to show time progression
+        :param plot_ind: list containing data indicies to plot
+        """
+        fig, ax_list = plt.subplots(1, len(plot_ind), sharey=True)
+        for ind_numb, (ind, ax) in enumerate(zip(plot_ind, ax_list)):
+            ax.set_title(f"T = {ind_numb*2}s")
+            self.plot_est_prob(ind, ax=ax)
+            ax.axis('off')
+        plt.tight_layout()
         plt.show()
 
+
     def plot_raw(self, datslice):
+        """
+        Plot the raw recorded data
+        """
         plt.plot(self.sdat_list[datslice])
         plt.show()
         
     def plot_est_pos(self):
         plt.matshow(self.map_room)
-        #self.est_pos=self.rl.precalc_dist[0]
         plt.scatter(self.est_pos[:,1], self.est_pos[:,0])
         plt.show()
 
@@ -116,13 +158,10 @@ def winAvg(data, winWidth=0, winfunc=np.blackman, mode="same"):
     data = np.convolve(data, kernel, mode=mode)
     return data
 
-#dslice = slice(500, 3000)
-dslice = slice(0, 280)
-est = []
+
 rbtest = RBLocTester("rb_datalog_phi.csv")
 rbtest.preprocess_locdata()
-rbtest.est_prob_video(dslice)
+#rbtest.est_prob_video(dslice)
 #rbtest.plot_raw(dslice)
 #rbtest.estloc_datarange(dslice)
-#rbtest.plot_est_pos()
-#rbtest.plot_est_prob()
+rbtest.plot_prob_timeline([220, 240, 260, 280])
