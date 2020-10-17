291d2cd600797ebf35c4b53c26f2d20d1e7e8f91
Alex Netsch <alex.netsch97@gmail.com>
2020-10-17 16:25:29 +0200

    Add sensor arduino code to transmit sensor data only (no motor control)

---
+++ mobile/SMFC_mobile_sense/SMFC_mobile_sense.ino
@@ -0,0 +1,146 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <arduino.h>
+#include "USDS.h"
+#include <MPU9250_asukiaaa.h>
+
+
+// PIN constants to avoid confusion
+static const uint8_t D0   = 16;
+static const uint8_t D1   = 5;
+static const uint8_t D2   = 4;
+static const uint8_t D3   = 0;
+static const uint8_t D4   = 2;
+static const uint8_t D5   = 14;
+static const uint8_t D6   = 12;
+static const uint8_t D7   = 13;
+static const uint8_t D8   = 15;
+static const uint8_t D9   = 3;
+static const uint8_t D10 = 1;
+
+#define SDA_PIN D4
+#define SCL_PIN D5
+
+//imu
+MPU9250_asukiaaa IMU;
+float mX, mY, mZ;
+byte magXY[4] = {0};
+
+
+// Wifi variables
+const char* ssid     = "LSRCT";
+const char* password = "83067046472468411597";
+const char* MQTT_BROKER = "192.168.178.27";
+
+
+// setup functions
+void setupWifi();
+//void setupIMU();
+void setupMQTT();
+void publish_mag();
+
+// MQTT stuff
+void reconnectMQTT();
+WiFiClient espClient;
+PubSubClient MQTTclient(espClient);
+
+// Sensor stuff
+// trigger, echo, trigger2, echo2,...
+USDS distSensor(D2, D1, D3, D0, D7, D6);
+
+void setup() {
+  Serial.begin(115200);
+  //setupIMU();
+  setupWifi();
+  setupMQTT();
+}
+
+void reconnectMQTT() {
+  /*
+   * Connect to the MQTT broker
+   */
+    while (!MQTTclient.connected()) {
+        Serial.println("Reconnecting MQTT...");
+        if (!MQTTclient.connect("ESP8266Client_sense")) {
+            Serial.print("failed, rc=");
+            Serial.print(MQTTclient.state());
+            Serial.println(" retrying in 5 seconds");
+            delay(5000);
+        } 
+        }
+    Serial.println("MQTT Connected...");
+}
+
+void setupMQTT(){
+  MQTTclient.setServer(MQTT_BROKER, 1883);
+}
+
+void setupIMU(){
+  /*
+   * Setup the IMU pins, wire connection and magnetometer sampling rate
+   */
+  Wire.begin(SDA_PIN, SCL_PIN);
+  IMU.setWire(&Wire);
+  IMU.beginMag(MAG_MODE_CONTINUOUS_100HZ);
+}
+
+void setupWifi(){
+  /*
+   * Connect to the wifi network
+   */
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  // set ESP to be wifi client
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  }
+  
+void loop() {
+  if (!MQTTclient.connected()) {
+        reconnectMQTT();
+  }
+  MQTTclient.loop();
+  distSensor.getDist();
+
+  MQTTclient.publish("RR/sensors", distSensor.dist, 6);
+ // publish_mag();
+   delay(80);
+}
+
+void publish_mag(){
+  /*
+   *Publish magnetometer X and Y to "RR/magnetometer"
+   *Encoded as 4 bytes, offset so its not negative
+   *Averages over 100 ms -> this function takes 100 ms
+  */
+  mX = 0;
+  mY = 0;
+  // avg over 100 ms
+  for(int i=0; i<10;i++){
+    IMU.magUpdate();
+    //mX += IMU.magHorizDirection()+180;
+    mX += IMU.magX();
+    mY += IMU.magY();
+    delay(10);
+    }
+  // make sure its not negativem 200 is arbitrary
+  mY = (mY)+2000;
+  mX = (mX)+2000;
+  magXY[0] = int(mX)>> 8;
+  magXY[1] = int(mX);
+  magXY[2] = int(mY)>> 8;
+  magXY[3] = int(mY);
+  MQTTclient.publish("RR/magnetometer", magXY, 4);
+  }
+
+
+  
---
+++ mobile/SMFC_mobile_sense/USDS.cpp
@@ -0,0 +1,65 @@
+/*
+  HCSR04 - Library for arduino, for HC-SR04 ultrasonic distance sensor.
+  Created by Martin Sosic, June 11, 2016.
+  Modified by AN
+*/
+
+#include "Arduino.h"
+#include "USDS.h"
+
+USDS::USDS(int triggerPin, int echoPin, int triggerPin2, int echoPin2, int triggerPin3, int echoPin3) {
+  this->triggerPin = triggerPin;
+  this->echoPin = echoPin;
+  this->triggerPin2 = triggerPin2;
+  this->echoPin2 = echoPin2;
+  this->triggerPin3 = triggerPin3;
+  this->echoPin3 = echoPin3;
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(triggerPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(triggerPin3, OUTPUT);
+  pinMode(echoPin3, INPUT);
+}
+
+void USDS::getDist() {
+    //Using the approximate formula 19.307°C results in roughly 343m/s which is the commonly used value for air.
+    return getDist(19.307);
+}
+
+void USDS::getDist(float temperature) {
+  // Make sure that trigger pin is LOW.
+  digitalWrite(triggerPin, LOW);
+  digitalWrite(triggerPin2, LOW);
+  digitalWrite(triggerPin3, LOW);
+  delayMicroseconds(2);
+   
+  // Hold trigger for 10 microseconds, which is signal for sensor to measure distance.
+  digitalWrite(triggerPin, HIGH);
+  digitalWrite(triggerPin2, HIGH);
+  digitalWrite(triggerPin3, HIGH);
+  delayMicroseconds(10);
+  // Measure the length of echo signal, which is equal to the time needed for sound to go there and back.
+  digitalWrite(triggerPin, LOW);
+  double durationMicroSec = (double)(pulseIn(echoPin, HIGH, 200000));
+  digitalWrite(triggerPin2, LOW);
+  double durationMicroSec2 = (double)(pulseIn(echoPin2, HIGH, 200000)); 
+  digitalWrite(triggerPin3, LOW);
+  double durationMicroSec3 = (double)(pulseIn(echoPin3, HIGH, 200000)); 
+  
+  double speedOfSoundInCmPerMs = 0.03313 + 0.0000606 * temperature; // Cair ≈ (331.3 + 0.606 ⋅ ϑ) m/s
+  
+  double distanceCm = ((durationMicroSec / 2.0 * speedOfSoundInCmPerMs)*10.0);
+  double distanceCm2 = ((durationMicroSec2 / 2.0 * speedOfSoundInCmPerMs)*10.0);
+  double distanceCm3 = ((durationMicroSec3 / 2.0 * speedOfSoundInCmPerMs)*10.0);
+
+  this->dist[0] = (int(distanceCm)>> 8)& 0xFF;
+  this->dist[1] = (int(distanceCm))& 0xFF;
+  this->dist[2] = (int(distanceCm2)>> 8)& 0xFF;
+  this->dist[3] = (int(distanceCm2))& 0xFF;
+  this->dist[4] = (int(distanceCm3)>> 8)& 0xFF;
+  this->dist[5] = (int(distanceCm3))& 0xFF;
+  
+  //this->dist[0] = (long)(distanceCm2*10) << 16 | (long)(distanceCm*10);
+  //this->dist[1] = (long)(distanceCm3*10);
+}
